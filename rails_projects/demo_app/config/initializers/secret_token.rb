14ba9b57cda2f4c003c210b6b8c172c28aaf00c2
Albert Hu <alberthu16@gmail.com>
2014-07-27 01:54:11 -0700

    initialize demo_app, (done setting up environment) learning ruby now

---
+++ rails_projects/demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '7fed0ddba8739769a3b87e03cc7d7e9bedb6fc02eb75422ad518e12411b60108c626be78e933503bd7ba24f869142615dc990bfa5ad629c55e0d048ec884dc94'
